db1f8d61032576889d17c74a5241731023c20ad5
BurrowedInCode <coffee_gopher@proton.me>
2026-06-20 03:36:46 -0400

    wire up handler for login

--- server/internal/auth/handler.go
+++ server/internal/auth/handler.go
@@ -4,9 +4,15 @@ import (
 	"encoding/json"
 	"log/slog"
 	"net/http"
+	"time"
 )
 
 type RegisterUserInput struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+type LoginInput struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
 }
@@ -18,7 +24,7 @@ func RegisterUserHandler(logger *slog.Logger, store AuthStore) http.HandlerFunc 
 		var input RegisterUserInput
 
 		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
-			http.Error(w, "Invalid body request", http.StatusBadRequest)
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
 			return
 		}
 
@@ -47,4 +53,51 @@ func RegisterUserHandler(logger *slog.Logger, store AuthStore) http.HandlerFunc 
 		}
 		w.WriteHeader(http.StatusCreated)
 	}
+}
+
+func LoginUserHandler(logger *slog.Logger, store AuthStore, secret string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
+		var input LoginInput
+
+		if err := json.NewDecoder(r.Body).Decode(&input); err != nil {
+			http.Error(w, "Invalid request body", http.StatusBadRequest)
+			return
+		}
+
+		user, err := store.GetUserByUserName(r.Context(), input.Username)
+		if err != nil {
+			http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
+			return
+		}
+
+		if err := VerifyPassword(user.PasswordHash, input.Password); err != nil {
+			http.Error(w, "Invalid login credentials", http.StatusUnauthorized)
+			return
+		}
+
+		token, err := GenerateToken(TokenConfig{
+			Username: user.Username,
+			Secret:   secret,
+			Duration: 24 * time.Hour,
+		})
+		if err != nil {
+			logger.Error("failed to generate token", "error", err)
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+
+		http.SetCookie(w, &http.Cookie{
+			Name:     "__Secure-token",
+			Value:    token,
+			HttpOnly: true,
+			Path:     "/",
+			SameSite: http.SameSiteStrictMode,
+			Secure:   true,
+		})
+
+		w.WriteHeader(http.StatusOK)
+	}
+
 }
