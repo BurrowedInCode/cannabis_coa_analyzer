226d68461146aa30f23537b0dbb5ede82fe3004c
BurrowedInCode <coffee_gopher@proton.me>
2026-06-23 02:41:01 -0400

    add types for coa schema

---
+++ client/src/types.ts
@@ -0,0 +1,35 @@
+export interface Laboratory {
+  name: string
+  address: string
+  phone: string
+  certification: string
+}
+
+export interface Cannabinoid {
+  name: string
+  value: number
+  unit: string
+}
+
+export interface Terpene {
+  name: string
+  value: number
+  unit: string
+}
+
+export interface TestSummary {
+  name: string
+  status: string
+}
+
+export interface Analysis {
+  laboratory: Laboratory
+  sample_name: string
+  seed_to_sale_number: string
+  sample_matrix: string
+  test_date: string
+  overall_pass: boolean
+  cannabinoids: Cannabinoid[]
+  terpenes: Terpene[]
+  summary: TestSummary[]
+}
