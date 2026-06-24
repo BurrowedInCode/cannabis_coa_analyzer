35b49cd197543cff7c62143da1afb4a7e8bb7fd2
BurrowedInCode <coffee_gopher@proton.me>
2026-06-24 03:50:34 -0400

    add new function to fetch analyses summary

--- client/src/api/coa.ts
+++ client/src/api/coa.ts
@@ -1,4 +1,4 @@
-import type { Analysis } from "@/types"
+import type { Analysis, AnalysisSummary } from "@/types"
 const BASE_URL = import.meta.env.VITE_API_URL
 
 export async function analyzeCOA(file: File | null): Promise<Analysis> {
@@ -23,4 +23,20 @@ export async function analyzeCOA(file: File | null): Promise<Analysis> {
   return res.json()
 }
 
+export async function getAllCOAAnalyses(limit: number, offset: number): Promise<AnalysisSummary[]> {
+  const res = await fetch(`${BASE_URL}/coa/analyses?limit=${limit}&offset=${offset}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/json"
+    },
+    credentials: "include",
+  })
+
+  if (!res.ok) {
+    const message = await res.text()
+    throw new Error(message)
+  }
+
+  return res.json()
+}
 
